961cd6427938e2c5ba3cd844330450a97340b44b
habib610 <habib54562@gmail.com>
2022-10-16 23:30:07 +0600

    container-loader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,11 @@
-import { Table } from "antd";
+import { Avatar, Table } from "antd";
 import { useEffect, useState } from "react";
 import "./App.css";
+import Container from "./components/Container";
+import Loader from "./components/Loader";
 import { EMPTY_MSG, ERR_MSG } from "./constants/constants";
 import request from "./requests/request";
 import { STUDENTS_ENDPOINT } from "./routes/routes";
-
-const columns = [
-    {
-        title: "Student Id",
-        dataIndex: "studentId",
-        key: "studentId",
-    },
-    {
-        title: "First Name",
-        dataIndex: "firstName",
-        key: "firstName",
-    },
-    {
-        title: "Last Name",
-        dataIndex: "lastName",
-        key: "lastName",
-    },
-    {
-        title: "Email",
-        dataIndex: "email",
-        key: "email",
-    },
-    {
-        title: "Gender",
-        dataIndex: "gender",
-        key: "gender",
-    },
-];
 
 function App() {
     const [data, setData] = useState([]);
@@ -53,22 +27,63 @@ function App() {
         };
         fetchStudents();
     }, []);
+    const columns = [
+        {
+            title: "",
+            dataIndex: "avatar",
+            render: (text, data) => (
+                <Avatar size="large">
+                    {`${data.firstName.slice(0, 1).toUpperCase()}`}
+                    {`${data.lastName.slice(0, 1).toUpperCase()}`}
+                </Avatar>
+            ),
+        },
+        {
+            title: "Student Id",
+            dataIndex: "studentId",
+            key: "studentId",
+        },
+        {
+            title: "First Name",
+            dataIndex: "firstName",
+            key: "firstName",
+        },
+        {
+            title: "Last Name",
+            dataIndex: "lastName",
+            key: "lastName",
+        },
+        {
+            title: "Email",
+            dataIndex: "email",
+            key: "email",
+        },
+        {
+            title: "Gender",
+            dataIndex: "gender",
+            key: "gender",
+        },
+    ];
 
     /* @DESC::  decide what to render  */
     let content = null;
     if (loading) {
-        content = <h1>Loading...</h1>;
+        content = <Loader />;
     } else if (!loading && error) {
         content = <h1>{error}</h1>;
     } else if (!loading && !error && data.length === 0) {
         content = <h1>{EMPTY_MSG}</h1>;
     } else if (!loading && !error && data.length > 0) {
-        content = <Table dataSource={data} columns={columns} />;
+        content = (
+            <Table pagination={false} dataSource={data} columns={columns} />
+        );
     }
     return (
-        <div className="App">
-            <h1>Hello World</h1>
-            {content}
+        <div>
+            <Container>
+                <h1>Hello World</h1>
+                {content}
+            </Container>
         </div>
     );
 }
