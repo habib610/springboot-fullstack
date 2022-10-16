647e83d3f1aefe7c097e06f1720c3be4c677189d
habib610 <habib54562@gmail.com>
2022-10-16 20:38:43 +0600

    initial-startup-application

---
+++ backend/src/main/java/com/rhabib/springfull/SpringfullApplication.java
@@ -0,0 +1,13 @@
+package com.rhabib.springfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringfullApplication.class, args);
+    }
+
+}
---
+++ backend/src/main/java/com/rhabib/springfull/student/StudentController.java
@@ -0,0 +1,32 @@
+package com.rhabib.springfull.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("students")
+public class StudentController {
+    @GetMapping
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(
+                        UUID.randomUUID(),
+                        "habib",
+                        "rahman",
+                        "habib1@gmail.com",
+                        Student.Gender.MALE
+                ),
+                new Student(
+                        UUID.randomUUID(),
+                        "Ema",
+                        "Watson",
+                        "ema@gmail.com",
+                        Student.Gender.FEMALE
+                )
+        );
+    }
+}
